7864e7642e05f994093a74ba7439a40be97a9340
dharam@epicenterlabs.com <dharam@epicenterlabs.com>
2016-12-19 17:05:06 +0530

    use combineReducers from redux

--- app/reducer/index.js
+++ app/reducer/index.js
@@ -1,20 +1,9 @@
+import {combineReducers} from 'redux'
 import {ADD_TO_CART,REMOVE_FROM_CART,CHANGE_CATEGORY} from '../components/constants/actionTypes.js'
 import activeCategory from './activeCategory.js'
 import products from './products.js'
 import shoppingCart from './shoppingCart.js'
 
-function reducer(state=initialState, action) {
-	return {
-		activeCategory: activeCategory(state.activeCategory,action),
-		products: products(state.products,action),
-		shoppingCart: shoppingCart(state.shoppingCart,action)
-	}
-}
-
-let s = reducer(initialState,{type: CHANGE_CATEGORY, payload: 'electronics'})
-console.log(s)
-s = reducer(initialState,{type: ADD_TO_CART, payload: 193})
-console.log(s)
-s = reducer(initialState,{type: REMOVE_FROM_CART, payload: 193})
-console.log(s)
+let reducer = combineReducers(activeCategory,products,shoppingCart)
+export default reducer
 
