e2973ca77a02be433c5a85566a6b62d04fb1ac00
dharam@epicenterlabs.com <dharam@epicenterlabs.com>
2016-12-19 15:35:00 +0530

    using redux store

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,40 +1,13 @@
-import React from 'react'
-import {incrementCount} from './actions'
+
+import React, {Component} from 'react'
+import { incrementCount } from '../actions'
+import  CountWidgetContainer  from '../containers/CountWidget.jsx'
 
 export default class App extends React.Component {
-	constructor(props) {
-		super(props)
-			this.state = {
-				count: props.store.getState() 
-			}
-		this.handleClick = this.handleClick.bind(this)
-		this.handleChange = this.handleChange.bind(this)
-	}
-
-	componentDidMount() {
-		this.props.store.subscribe(this.handleChange)
-	}
-
-	handleChange() {
-		this.setState({
-			count: this.props.store.getState()
-		})
-	}
-
-	handleClick() {
-		/*this.setState({
-			count: this.state.count + 1
-		})*/
-		let action = incrementCount()
-		this.props.store.dispatch(incrementCount())
-		//console.log(action)
-	}
-
 	render() {
 		return(
 				<div>
-				<p> Count: {this.state.count} </p>
-				<button onClick={this.handleClick}>Increment</button>
+				<CountWidgetContainer store={this.props.store}/>
 				</div>
 		      )
 	}
