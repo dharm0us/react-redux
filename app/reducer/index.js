0af5d2e8780a1e1d78ab1ad9957a70e5c4bdc60f
dharam@epicenterlabs.com <dharam@epicenterlabs.com>
2016-12-19 17:02:04 +0530

    splitting reducers

--- app/reducer/index.js
+++ app/reducer/index.js
@@ -1,36 +1,13 @@
 import {ADD_TO_CART,REMOVE_FROM_CART,CHANGE_CATEGORY} from '../components/constants/actionTypes.js'
+import activeCategory from './activeCategory.js'
+import products from './products.js'
+import shoppingCart from './shoppingCart.js'
 
-let initialState = {
-	activeCategory: 'food',
-	products: [
-	{id:1, name:'pizza', cost:10, category:'food'},
-	{id:2, name:'apple', cost:20, category:'food'},
-	{id:3, name:'tv', cost:50, category:'electronics'},
-	{id:4, name:'phone', cost:500, category:'phone'},
-		],
-	shoppingCart: [
-		]
-}	
-
-export default function reducer(state=initialState, action) {
-	switch(action.type) {
-		case CHANGE_CATEGORY:
-			return {
-				...state,
-				activeCategory: action.payload
-			}
-		case ADD_TO_CART:
-			return {
-				...state,
-				shoppingCart: [...state.shoppingCart, action.payload]
-			}
-		case REMOVE_FROM_CART:
-			return {
-				...state,
-				shoppingCart: state.shoppingCart.filter(productId => productId != action.payload)
-			}
-		default:
-			return state
+function reducer(state=initialState, action) {
+	return {
+		activeCategory: activeCategory(state.activeCategory,action),
+		products: products(state.products,action),
+		shoppingCart: shoppingCart(state.shoppingCart,action)
 	}
 }
 
